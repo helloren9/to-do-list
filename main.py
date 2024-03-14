2abfed51d267408f9aa68c08ed53d09a0093b486
Loren Anunciacion <loren.anunciacion@gmail.com>
2024-03-14 10:45:26 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -8,6 +8,42 @@ root .resizable(False, False)
 
 task_list = []
 
+def addTask():
+    task = task_entry.get()
+    task_entry.delete(0, END)
+
+    if task:
+        with open('tasklist.txt', 'a') as taskfile:
+            taskfile.write(f"\n{task}")
+        task_list.append(task)
+        listbox.insert(END, task)
+
+def deleteTask():
+    global task_list
+    task = str(listbox.get(ANCHOR))
+    if task in task_list:
+        task_list.remove(task)
+        with open("tasklist.txt","w") as taskfile:
+            for task in task_list:
+                taskfile.write(task + "\n")
+
+        listbox.delete(ANCHOR)
+
+def openTaskFile():
+        try:
+            global task_list
+            with open("tasklist.txt","r") as taskfile:
+                tasks = taskfile.readlines()
+
+            for task in tasks:
+                if task != '\n':
+                    task_list.append(task)
+                    listbox.insert(END, task)
+
+        except:
+            file=open('tasklist.txt', 'w')
+            file.close()
+
 Image_icon = PhotoImage(file="images/checklist.png")
 root.iconphoto(False, Image_icon)
 
@@ -28,13 +64,13 @@ task_entry = Entry(frame, width=22, font="arial 20", bd=0)
 task_entry.place(x=10, y=7)
 task_entry.focus()
 
-button = Button(frame, text="Add", font="arial 20 bold", width=6, bg="lightgray", fg="#fff", bd=0)
+button = Button(frame, text="Add", font="arial 20 bold", width=6, bg="lightgray", fg="#fff", bd=0, command=addTask)
 button.place(x=236, y=0)
 
 frame1 = Frame(root, bd=3, width = 260, height = 250, bg = "black")
 frame1.pack(pady=(50,0))
 
-listbox = Listbox(frame1, font=('arial', 12), width = 37, height = 16, bg = "lightgray", fg="white", cursor="hand2", selectbackground="white")
+listbox = Listbox(frame1, font=('arial', 12), width = 37, height = 16, bg = "lightgray", fg="black", cursor="hand2", selectbackground="gray")
 listbox.pack(side=LEFT, padx=0)
 scrollbar = Scrollbar(frame1)
 scrollbar.pack(side=RIGHT, fill=BOTH)
@@ -42,7 +78,9 @@ scrollbar.pack(side=RIGHT, fill=BOTH)
 listbox.config(yscrollcommand=scrollbar.set)
 scrollbar.config(command=listbox.yview)
 
+openTaskFile()
+
 Delete_icon = PhotoImage(file="images/trash-bin.png")
-Button(root, image=Delete_icon, bd = 0).place(x=140, y=520)
+Button(root, image=Delete_icon, bd = 0, command=deleteTask).place(x=140, y=520)
 
 root.mainloop()
